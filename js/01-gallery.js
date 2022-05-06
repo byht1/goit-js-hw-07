3659a9ef6adddc4607191d0cb19bb069dee1ccf8
byht1 <vitalikbyht1@gmail.com>
2022-05-06 23:14:48 +0300

    exercise 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// ________________ Конструктор __________________
+
+const gallery = document.querySelector('.gallery');
+const imgGallery = galleryItems
+  .map(x => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href='${x.original}'>
+    <img
+      class="gallery__image"
+      src="${x.preview}"
+      data-source="${x.original}"
+      alt="${x.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', imgGallery);
+
+// __________ Делегирование __________________
+
+gallery.addEventListener('click', eve => {
+  eve.preventDefault();
+  if (eve.target.closest('.gallery__link')) {
+    const instance = basicLightbox.create(
+      `<img src="${event.target.closest('img').dataset.source}" width="800" height="600">`
+    );
+    instance.show();
+  }
+  false;
+});
+
+// ______________Кнопка_______________
+
+document.addEventListener('keyup', eve => {
+  const isEscape = eve.code === 'Escape';
+  const isActiv = document.querySelector('.basicLightbox');
+  if (!isActiv) {
+    return;
+  }
+  if (isEscape) {
+    isActiv.remove();
+  }
+});
