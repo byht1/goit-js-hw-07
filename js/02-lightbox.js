66ae9497b7e69371d95fbe66833b64d59de74672
byht1 <vitalikbyht1@gmail.com>
2022-05-07 14:28:47 +0300

    завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// ____________________Галерея________________________
+
+const constructor = document.querySelector('.gallery');
+const galleryImg = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a></li>`;
+  })
+  .join('');
+
+constructor.insertAdjacentHTML('beforeend', galleryImg);
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', () => {
+  gallery.defaultOptions.captionDelay = 250;
+});
